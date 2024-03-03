1b0e00ea0f0bd19d25c831dd749b5ab9fcc6ccca
Sahil Kumar <119723019+SahilKumar000@users.noreply.github.com>
2024-03-03 14:22:20 +0530

    Update Prefix_Function.cpp

--- String Algorithms/Prefix_Function.cpp
+++ String Algorithms/Prefix_Function.cpp
@@ -2,12 +2,10 @@ vector<int> kmp(string s) {
     int n = s.length();
     vector<int> kmp(n,0);
     for (int i = 1; i < n; i++) {
-        int j = kmp[i-1];
-        while (j > 0 && s[i] != s[j])
-            j = kmp[j-1];
-        if (s[i] == s[j])
-            j++;
-        pi[i] = j;
-    }
-    return pi;
+        int prev_index = kmp[i-1];
+        while (prev_index > 0 && s[i] != s[j])
+            prev_index = kmp[j-1];
+        kmp[i] = prev_index + (s[i]==s[prev_index])?1:0;
+     }
+    return kmp;
 }
